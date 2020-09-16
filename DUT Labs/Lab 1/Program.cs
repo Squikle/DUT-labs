ffe8f35f24b696347cb311a0f3ac18e4bfe2d479
Squikle <55331475+Squikle@users.noreply.github.com>
2020-09-16 22:21:47 +0300

    Added

---
+++ DUT Labs/Lab 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.HtmlControls;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите высоту конуса: ");
+            double height = Double.Parse(Console.ReadLine());
+
+            Console.Write("Введите радиус конуса: ");
+            double radius = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine((Math.PI*Math.Pow(radius, 2)*height)/3);
+            Console.ReadKey();
+        }
+    }
+}

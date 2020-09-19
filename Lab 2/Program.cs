d65f84bd5ef4e0eda5cd7299e7a954c68cfc11c1
Squikle <55331475+Squikle@users.noreply.github.com>
2020-09-19 23:26:32 +0300

    Lab 2 added

--- Lab 2/Program.cs
+++ Lab 2/Program.cs
@@ -1,16 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
-namespace Lab_2
+namespace main
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            int start;
+            int finish;
+
+            Console.WriteLine("Введите стартовый элемент (целое число): ");
+            string start_index_str = Console.ReadLine();
+            finish = Convert.ToInt32(start_index_str);
+
+            Console.WriteLine("Введите финишный элемент (целое число): ");
+            string finish_index_str = Console.ReadLine();
+            start = Convert.ToInt32(finish_index_str);
+
+            double summ = 0;
+            if (finish <= start && finish >= 0)
+            {
+                for (int i = finish; i < start; i++)
+                {
+                    double currentI = (Math.Pow(i, 2) + Math.Pow(-1, i - 1) * 2 * i - 1) / (Math.Pow(i, 2) + 8);
+                    summ += currentI;
+                    Console.WriteLine(currentI);
+                }
+                Console.WriteLine("Сумма ряда: {0}", summ); ;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка. Некорректные числа");
+            }
+
+            Console.ReadKey();
         }
     }
 }

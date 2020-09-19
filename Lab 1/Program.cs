8dc74094e7c7ab08e600cfdc431377d7332e4246
Squikle <55331475+Squikle@users.noreply.github.com>
2020-09-19 23:44:22 +0300

    Revert "Lab 3 added, 1,2 edited"
    This reverts commit 8944c349c322cfc95bb71e9eb49fa9e498189e67.

--- Lab 1/Program.cs
+++ Lab 1/Program.cs
@@ -1,4 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.HtmlControls;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Globalization;
 
 namespace Lab_1
 {

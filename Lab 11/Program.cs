7e365d3930ce197a27c819e06d57f3138f4ce8f6
Squikle <55331475+Squikle@users.noreply.github.com>
2020-12-08 19:23:21 +0200

    finished lab 11

--- Lab 11/Program.cs
+++ Lab 11/Program.cs
@@ -30,6 +30,20 @@ namespace Lab_11
             Console.WriteLine();
             DoWheelService(carService).Invoke(car3);
             Console.WriteLine(car3);
+            Console.WriteLine();
+
+            Console.WriteLine("\tCar4 own service request:\n");
+            Car car4 = new Car();
+            Console.WriteLine(car4);
+            Console.WriteLine();
+            CarService.ServiceDelegate operation = RequestCustomService(carService);
+            if (operation != null)
+            {
+                operation.Invoke(car4);
+                Console.WriteLine(car4);
+            }
+            else
+                Console.WriteLine("Работы не были проведены");
 
             Console.ReadKey();
         }
@@ -58,6 +72,85 @@ namespace Lab_11
             serviceOperation += carService.ChangeOil;
             serviceOperation += carService.ReplaceWheels;
             serviceOperation += carService.TuneWheelAlignment;
+            return serviceOperation;
+        }
+        static CarService.ServiceDelegate RequestCustomService(CarService carService)
+        {
+            CarService.ServiceDelegate serviceOperation = null;
+            ConsoleKey response;
+            while (true)
+            {
+                Console.Write("Хотите сделать развал-схождение? [y/n]");
+                response = Console.ReadKey(false).Key;
+                if (response == ConsoleKey.Y)
+                    serviceOperation += carService.TuneWheelAlignment;
+                else if (response != ConsoleKey.N)
+                    continue;
+
+                Console.WriteLine();
+                break;
+            }
+            while (true)
+            {
+                Console.Write("Хотите поменять масло? [y/n]");
+                response = Console.ReadKey(false).Key;
+                if (response == ConsoleKey.Y)
+                    serviceOperation += carService.ChangeOil;
+                else if (response != ConsoleKey.N)
+                    continue;
+
+                Console.WriteLine();
+                break;
+            }
+            while (true)
+            {
+                Console.Write("Хотите сделать покраску? [y/n]");
+                response = Console.ReadKey(false).Key;
+                if (response == ConsoleKey.Y)
+                    serviceOperation += carService.Paint;
+                else if (response != ConsoleKey.N)
+                    continue;
+
+                Console.WriteLine();
+                break;
+            }
+            while(true)
+            {
+                Console.Write("Хотите сделать полный техосмотр?");
+                response = Console.ReadKey(false).Key;
+                if (response == ConsoleKey.Y)
+                    serviceOperation += carService.FullInspect;
+                else if (response != ConsoleKey.N)
+                    continue;
+
+                Console.WriteLine();
+                break;
+            }
+            while (true)
+            {
+                Console.Write("Хотите заменить колеса?");
+                response = Console.ReadKey(false).Key;
+                if (response == ConsoleKey.Y)
+                    serviceOperation += carService.ReplaceWheels;
+                else if (response != ConsoleKey.N)
+                    continue;
+
+                Console.WriteLine();
+                break;
+            }
+            while(true)
+            {
+                Console.Write("Хотите починить кузов?");
+                response = Console.ReadKey(false).Key;
+                if (response == ConsoleKey.Y)
+                    serviceOperation += carService.RepairBody;
+                else if (response != ConsoleKey.N)
+                    continue;
+
+                Console.WriteLine();
+                break;
+            }
+
             return serviceOperation;
         }
     }

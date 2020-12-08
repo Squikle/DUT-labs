6e21c02a4c946310daacb1a535ae66858d0c9943
Squikle <55331475+Squikle@users.noreply.github.com>
2020-12-08 20:21:56 +0200

    added 11.2 lab

---
+++ Lab 11.2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Lab_11._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Student> students = new List<Student>();
+            Random rand = new Random();
+
+            int numberOfStudents = 15;
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                students.Add(new Student()
+                {
+                    Age = rand.Next(15, 25),
+                    FirstName = GetRandomFirstName(),
+                    LastName = GetRandomLastName()
+                });
+                Thread.Sleep(10);
+            }
+                
+
+            Console.WriteLine("Все студенты:");
+            students.ForEach(Console.WriteLine);
+            Console.WriteLine();
+
+            Console.WriteLine("Старше 18");
+            students.FindStudents(Student.AgeCheck).ForEach(Console.WriteLine);
+            Console.WriteLine();
+            Console.WriteLine("Первая буква имени \"А\"");
+            students.FindStudents(Student.FirstNameFirstLetterA).ForEach(Console.WriteLine);
+            Console.WriteLine();
+            Console.WriteLine("Фамилия длинне 3 букв");
+            students.FindStudents(Student.LastNameIsLongerThan3).ForEach(Console.WriteLine);
+            Console.WriteLine();
+
+            Console.WriteLine("Старше 18");
+            students.FindStudents(s => s.Age >= 18).ForEach(Console.WriteLine);
+            Console.WriteLine();
+            Console.WriteLine("Первая буква имени \"А\"");
+            students.FindStudents(s => s.FirstName?[0] == 'a').ForEach(Console.WriteLine);
+            Console.WriteLine();
+            Console.WriteLine("Фамилия длинне 3 букв");
+            students.FindStudents(s => s.LastName?.Length > 3).ForEach(Console.WriteLine);
+            Console.WriteLine();
+
+            Console.WriteLine("Возраст от 20 до 25");
+            students.FindStudents(s => s.Age >= 20 && s.Age <= 25).ForEach(Console.WriteLine);
+            Console.WriteLine();
+            Console.WriteLine("Имя \"Andrew\"");
+            students.FindStudents(s => s.FirstName.Equals("Andrew")).ForEach(Console.WriteLine);
+            Console.WriteLine();
+            Console.WriteLine("Фамилия \"Troelsen\"");
+            students.FindStudents(s => s.LastName.Equals("Troelsen")).ForEach(Console.WriteLine);
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+
+        private static string GetRandomFirstName()
+        {
+            string[] names =
+            {
+                "James",
+                "Andrew",
+                "Robert",
+                "Michael",
+                "William",
+                "Albert",
+                "Elizabeth",
+                "Adam",
+                "Karen",
+                "Jennifer",
+                "Lisa",
+                "Nancy",
+            };
+            return names[new Random().Next(names.Length)];
+        }
+        private static string GetRandomLastName()
+        {
+            string[] names =
+            {
+                "Baccari",
+                "Zui",
+                "MarBeltrancia",
+                "Vang",
+                "Baalman",
+                "Lan",
+                "Troelsen",
+                "Moreno",
+                "Collins",
+                "Deleon",
+                "Anderson",
+                "Beltran",
+            };
+            return names[new Random().Next(names.Length)];
+        }
+    }
+}
---
+++ Lab 11.2/Student.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab_11._2
+{
+    public class Student
+    {
+        public delegate bool StudentPredicateDelegate(Student student);
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public static bool AgeCheck(Student student) => student.Age >= 18;
+        public static bool FirstNameFirstLetterA(Student student) => student.FirstName?[0] == 'a';
+        public static bool LastNameIsLongerThan3(Student student) => student.LastName?.Length > 3;
+
+        public override string ToString()
+        {
+            return $"Имя: {FirstName}" +
+                   $" Фамилия: {LastName}" +
+                   $" Возраст: {Age}";
+        }
+    }
+}

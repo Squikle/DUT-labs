bedd3000571d6b2b7923e4b16d92dce00c23b983
Squikle <55331475+Squikle@users.noreply.github.com>
2020-12-08 17:06:57 +0200

    Lab 10 added

---
+++ Lab 10/Extensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Lab_10
+{
+    public static class Extensions
+    {
+       public static int Reverse(this int value)
+        {
+            int reversed = 0;
+            while (value > 0)
+            {
+                int remainded = value % 10;
+                reversed = (reversed * 10) + remainded;
+                value = value / 10;
+            }
+            return reversed;
+        }
+       public static string Reverse(this string value)
+        {
+            string reversed = string.Empty;
+            for (int i = value.Length - 1; i >= 0; i--)
+                reversed += value[i];
+            return reversed;
+        }
+       public static string SplitReverse(this string value)
+        {
+            string reversed = string.Empty;
+            var strings = value.Split(',');
+
+            for (int i = 0; i < strings.Length - 1; i++)
+                reversed += Reverse(strings[i]) + ',';
+
+            reversed += Reverse(strings[strings.Length - 1]);
+
+            return reversed;
+        }
+       public static double SplitReverse(this double value) =>
+            Double.Parse(SplitReverse(value.ToString(CultureInfo.CurrentCulture)));
+       public static T[] Reverse<T>(this T[] array)
+        {
+            T[] newArr = new T[array.Length];
+            for (int i = 0, j = array.Length - 1;
+                i < array.Length;
+                i++, j--)
+            {
+                newArr[i] = array[j];
+            }
+
+            return newArr;
+        }
+       public static void PrintArrayEvenOdd(this int[] array)
+       {
+           List<int> oddElements = array.Where(el => el % 2 == 0).ToList();
+           oddElements.ForEach(el => Console.Write(el + " "));
+           array.Except(oddElements).ToList().ForEach(el => Console.Write(el + " "));
+       }
+    }
+}
---
+++ Lab 10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            int task1 = 123;
+            Console.WriteLine("task 1:\t\t" + task1.Reverse());
+
+            // Task 2
+            string task2 = "test";
+            Console.WriteLine("task 2:\t\t" + task2.Reverse());
+
+            // Task 3
+            double task3 = 12.321;
+            Console.WriteLine("task 3:\t\t" + Convert.ToDouble(task3.SplitReverse()));
+
+            // Task 4
+            string task4 = "sa,ffdd,afdss,qgasfg,54";
+            Console.WriteLine("task 4:\t\t" + task4.SplitReverse());
+
+            // Task 7
+            Console.Write("task 7:\t\t");
+            int[] task7 = new int[10] { 1, 3, 5, 1, 6, 3, 2, 1, 6, 10 };
+            var reversedArray = task7.Reverse();
+            for (int i = 0; i < reversedArray.Length; i++)
+                Console.Write(" " + reversedArray[i]);
+            Console.WriteLine();
+
+            // Variant 7 Task
+            int[] variantTask = {1, 3, 2, 4, 5, 6};
+            Console.Write("variant 7 task:\t\t ");
+            variantTask.PrintArrayEvenOdd();
+
+            Console.ReadKey();
+        }
+    }
+}
